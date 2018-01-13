441d0136ab82427cb3a092f9279bfe355b32dafc
JC Smiley <jcsmileyjr@gmail.com>
2018-01-12 16:29:32 -0800

    Combined science and mississippi studies terms

--- JS/vocabularyService.js
+++ JS/vocabularyService.js
@@ -17,7 +17,26 @@ myApp.factory('Vocabulary',  function () {
         {"term":"A meathod of expressing a quantity as a number mulitplied by 10 to the approparate power", "correctAnswer":"Scientific notation"},
         {"term":"The exactness of a measurement", "correctAnswer":"Precision"},
         {"term":"A prescribed decimal place that determines the amount of rounding of the measurement", "correctAnswer":"Signifant Figure"},
-        {"term":"A description of how close a measurement is to the true value of the quantity measured", "correctAnswer":"Accuracy"}
+        {"term":"A description of how close a measurement is to the true value of the quantity measured", "correctAnswer":"Accuracy"},
+        {"term":"On planet Earth", "correctAnswer":"Location"},
+        {"term":"has both physical characteristics and human, or cultural characteristics", "correctAnswer":"Place"},        
+        {"term":"describes how humans use and interact with the enviroment", "correctAnswer":"Human-enviroment interaction"},
+        {"term":"concerns humans interactions the way we are linked with regions, cultures, and people beyond our immediate envirment", "correctAnswer":"Movement"},
+        {"term":"Geographers divide the world", "correctAnswer":"Regions"},
+        {"term":"Is the distance north and south of the equatar", "correctAnswer":"Latitude"},
+        {"term":"Is the distance east or west of the prime meridian at Greenwich, England", "correctAnswer":"Longitude"},
+        {"term":"Now helps us determine the precise location of a place", "correctAnswer":"Global Positioning System (GPS)"},
+        {"term":"includes such elements as climate, water, land forms, soil, energy, and mineral resource, vegtation, and different species of wildlife.", "correctAnswer":"Natural Environment"},
+        {"term":"refers to the long terms of temperature and rainfall", "correctAnswer":"Climate"},
+        {"term":"refers to the day-to-day conditions and changes in the atmosphere for temperature, rainfall, wind, cloudiness, humidly, and air pressure", "correctAnswer":"weather"},
+        {"term":"rain, snow, sleet, and hail is 52 inches", "correctAnswer":"precipitation"},
+        {"term":"generally occur during the spring and early summer when warm moist air moving northward from the Gulf of Mexico meets colder air coming from the interior of the United States", "correctAnswer":"Tornadoes"},
+        {"term":"are storms that arise in the tropical Atlantic ocean and cover hundreads of square miles", "correctAnswer":"Hurricanes"},
+        {"term":"refers to the water in lakes, ponds, rivers, and the ocean", "correctAnswer":"Surface Water"},
+        {"term":"seeps below Earth's surface into the spaces and cracks in the rocks. Aquifiers: it slowly moves through and accumulates in rocks formation", "correctAnswer":"Ground Water"},
+        {"term":"Is in geographical area draining into a area or reservior", "correctAnswer":"Drain-age basin"},
+        {"term":"many people plant trees on Earth Day", "correctAnswer":"Left"},
+        {"term":"John Muir (right) and Teddy Roosevelt at Yosemite", "correctAnswer":"Above"}        
     ];
 
     // location object that points to the current location in the test array
